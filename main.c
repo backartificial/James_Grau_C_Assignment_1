da76e644c9b5755d92ae71d79351beabe151eb2f
James Grau <backartificial@users.noreply.github.com>
2018-05-25 13:07:53 -0400

    Initial Setup
    Initial Commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: james
+ *
+ * Created on May 25, 2018, 9:04 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+
+    return (EXIT_SUCCESS);
+}
+
